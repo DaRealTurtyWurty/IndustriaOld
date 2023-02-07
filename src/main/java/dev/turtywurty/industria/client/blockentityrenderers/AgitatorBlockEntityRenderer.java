bd6f73418e3cfa51bdcf339206f8270a4a895e48
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2023-02-07 19:59:06 +0000

    Agitator fix and block item models

--- src/main/java/dev/turtywurty/industria/client/blockentityrenderers/AgitatorBlockEntityRenderer.java
+++ src/main/java/dev/turtywurty/industria/client/blockentityrenderers/AgitatorBlockEntityRenderer.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 
 public class AgitatorBlockEntityRenderer implements BlockEntityRenderer<AgitatorBlockEntity> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(Industria.MODID,
@@ -23,17 +24,28 @@ public class AgitatorBlockEntityRenderer implements BlockEntityRenderer<Agitator
     @Override
     public void render(AgitatorBlockEntity pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay) {
         pPoseStack.pushPose();
-        pPoseStack.translate(0.5, 0, 0.5);
+        pPoseStack.translate(0.5, 0.5, 0.5);
         pPoseStack.scale(1.0F, -1.0F, -1.0F);
         this.model.renderToBuffer(pPoseStack, pBufferSource.getBuffer(this.model.renderType(TEXTURE)), pPackedLight,
                 pPackedOverlay, 1.0F, 1.0F, 1.0F, 1.0F);
         pPoseStack.popPose();
 
         pPoseStack.pushPose();
-        pPoseStack.translate(0.5, 0.5, 0.5);
+        pPoseStack.translate(0.5, 0.343745, 0.5);
         pPoseStack.scale(1.0F, -1.0F, -1.0F);
-        this.model.getParts().rotor().yRot += 0.1F;
-        this.model.getParts().rotor().translateAndRotate(pPoseStack);
+
+
+        if(pBlockEntity.getProgress() > 0) {
+            this.model.getParts().rotor().yRot += 0.01F;
+            this.model.getParts().rotor().translateAndRotate(pPoseStack);
+        } else {
+            float yRot = this.model.getParts().rotor().yRot;
+            if(yRot != 0) {
+                this.model.getParts().rotor().yRot = Mth.lerp(yRot, 360, 0);
+                this.model.getParts().rotor().translateAndRotate(pPoseStack);
+            }
+        }
+
         this.model.getParts().rotor()
                 .render(pPoseStack, pBufferSource.getBuffer(this.model.renderType(TEXTURE)), pPackedLight,
                         pPackedOverlay);
--- src/main/java/dev/turtywurty/industria/client/model/AgitatorModel.java
+++ src/main/java/dev/turtywurty/industria/client/model/AgitatorModel.java
@@ -27,79 +27,76 @@ public class AgitatorModel extends Model {
         PartDefinition partDefinition = meshDefinition.getRoot();
 
         PartDefinition main = partDefinition.addOrReplaceChild("main", CubeListBuilder.create(),
-                PartPose.offset(0.0F, 24.0F, 0.0F));
-
-        PartDefinition structure = main.addOrReplaceChild("structure", CubeListBuilder.create().texOffs(60, 8)
-                        .addBox(-4.0F, -7.5217F, -6.0F, 8.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(57, 58)
-                        .addBox(-4.0F, 7.4783F, -8.0F, 8.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(38, 58)
-                        .addBox(-4.0F, 7.4783F, 7.0F, 8.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
-                        .addBox(-8.0F, 7.4783F, -4.0F, 16.0F, 1.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(52, 0)
-                        .addBox(-5.0F, 7.4783F, 6.0F, 10.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 23)
-                        .addBox(-7.0F, 7.4783F, -5.0F, 14.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 20)
-                        .addBox(-7.0F, 7.4783F, 4.0F, 14.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(19, 51)
-                        .addBox(-5.0F, 7.4783F, -7.0F, 10.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(41, 16)
-                        .addBox(-6.0F, 7.4783F, 5.0F, 12.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(33, 13)
-                        .addBox(-6.0F, 7.4783F, -6.0F, 12.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(41, 53)
-                        .addBox(-4.0F, 4.4783F, -8.0F, 8.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 39)
-                        .addBox(7.0F, 4.4783F, -4.0F, 1.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(52, 34)
-                        .addBox(-4.0F, 4.4783F, 7.0F, 8.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(19, 35)
-                        .addBox(-8.0F, 4.4783F, -4.0F, 1.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(52, 29)
-                        .addBox(-4.0F, -4.5217F, -8.0F, 8.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 35)
-                        .addBox(7.0F, -4.5217F, -4.0F, 1.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(52, 3)
-                        .addBox(-4.0F, -4.5217F, 7.0F, 8.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 27)
-                        .addBox(-8.0F, -4.5217F, -4.0F, 1.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(41, 31)
-                        .addBox(6.0F, -6.5217F, -4.0F, 1.0F, 2.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(0, 57)
-                        .addBox(-4.0F, -6.5217F, -7.0F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(41, 2)
-                        .addBox(-7.0F, -6.5217F, -4.0F, 1.0F, 2.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(18, 54)
-                        .addBox(-4.0F, -6.5217F, 6.0F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(60, 61)
-                        .addBox(-6.0F, -4.5217F, -6.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(55, 61)
-                        .addBox(-5.0F, -4.5217F, -7.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(50, 61)
-                        .addBox(-7.0F, -4.5217F, -5.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(45, 61)
-                        .addBox(5.0F, -4.5217F, -6.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(40, 61)
-                        .addBox(4.0F, -4.5217F, -7.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(35, 61)
-                        .addBox(6.0F, -4.5217F, -5.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 61)
-                        .addBox(6.0F, -4.5217F, 4.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(25, 61)
-                        .addBox(-5.0F, -4.5217F, 6.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 24)
-                        .addBox(5.0F, -6.5217F, 4.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(22, 23)
-                        .addBox(-5.0F, -6.5217F, 5.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(18, 20)
-                        .addBox(-5.0F, -6.5217F, -6.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 20)
-                        .addBox(5.0F, -6.5217F, -5.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(20, 61)
-                        .addBox(5.0F, -4.5217F, 5.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(15, 61)
-                        .addBox(-6.0F, -4.5217F, 5.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 14)
-                        .addBox(4.0F, -6.5217F, 5.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 10)
-                        .addBox(-6.0F, -6.5217F, 4.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 4)
-                        .addBox(-6.0F, -6.5217F, -5.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
-                        .addBox(4.0F, -6.5217F, -6.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(10, 61)
-                        .addBox(4.0F, -4.5217F, 6.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(5, 61)
-                        .addBox(-7.0F, -4.5217F, 4.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 10)
-                        .addBox(-6.0F, -7.5217F, -4.0F, 12.0F, 1.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(48, 50)
-                        .addBox(-5.0F, -7.5217F, -5.0F, 10.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(41, 26)
-                        .addBox(-5.0F, -7.5217F, 4.0F, 10.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(19, 58)
-                        .addBox(-4.0F, -7.5217F, 5.0F, 8.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
-                PartPose.offset(0.0F, -8.4783F, 0.0F));
-
-        structure.addOrReplaceChild("glass", CubeListBuilder.create().texOffs(19, 20)
-                        .addBox(-8.0F, -2.5F, -19.0F, 1.0F, 6.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(49, 42)
-                        .addBox(-4.0F, -2.5F, -23.0F, 8.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 20)
-                        .addBox(7.0F, -2.5F, -19.0F, 1.0F, 6.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(0, 47)
-                        .addBox(-4.0F, -2.5F, -8.0F, 8.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)),
-                PartPose.offset(0.0F, 0.9783F, 15.0F));
+                PartPose.offset(0.0F, 16.0F, 0.0F));
 
         main.addOrReplaceChild("rotor", CubeListBuilder.create().texOffs(67, 35)
-                        .addBox(-0.5F, 3.6538F, -5.0F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(68, 17)
-                        .addBox(0.5F, 3.6538F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(67, 11)
-                        .addBox(-0.5F, 3.6538F, 0.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(65, 67)
-                        .addBox(-5.0F, 3.6538F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 61)
-                        .addBox(-0.5F, -8.3462F, -0.5F, 1.0F, 13.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(65, 61)
-                        .addBox(-0.5F, -0.3462F, -5.0F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(64, 25)
-                        .addBox(0.5F, -0.3462F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(60, 53)
-                        .addBox(-5.0F, -0.3462F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(61, 19)
-                        .addBox(-0.5F, -0.3462F, 0.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(60, 11)
-                        .addBox(0.5F, -4.3462F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(11, 35)
-                        .addBox(-0.5F, -4.3462F, -5.0F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(19, 47)
-                        .addBox(-5.0F, -4.3462F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(11, 20)
-                        .addBox(-0.5F, -4.3462F, 0.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)),
-                PartPose.offset(0.0F, -6.6538F, 0.0F));
+                        .addBox(-0.5F, -0.5F, 0.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(0, 61)
+                        .addBox(-0.5F, -4.5F, -0.5F, 1.0F, 13.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(67, 11)
+                        .addBox(-0.5F, 7.5F, 0.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(65, 61)
+                        .addBox(-0.5F, 7.5F, -4.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(68, 17)
+                        .addBox(0.5F, 7.5F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(65, 67)
+                        .addBox(-4.5F, 7.5F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(64, 25)
+                        .addBox(-4.5F, 3.5F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(61, 19)
+                        .addBox(-0.5F, 3.5F, -4.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(60, 53)
+                        .addBox(0.5F, 3.5F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(11, 35)
+                        .addBox(-0.5F, 3.5F, 0.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(60, 11)
+                        .addBox(-4.5F, -0.5F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(11, 20)
+                        .addBox(-0.5F, -0.5F, -4.5F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)).texOffs(19, 47)
+                        .addBox(0.5F, -0.5F, -0.5F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(0.0F, -2.5F, 0.0F));
+
+        main.addOrReplaceChild("structure", CubeListBuilder.create().texOffs(60, 8)
+                        .addBox(-4.0F, -0.5F, -6.0F, 8.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(57, 58)
+                        .addBox(-4.0F, 14.5F, -8.0F, 8.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(38, 58)
+                        .addBox(-4.0F, 14.5F, 7.0F, 8.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+                        .addBox(-8.0F, 14.5F, -4.0F, 16.0F, 1.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(52, 0)
+                        .addBox(-5.0F, 14.5F, 6.0F, 10.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 23)
+                        .addBox(-7.0F, 14.5F, -5.0F, 14.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 20)
+                        .addBox(-7.0F, 14.5F, 4.0F, 14.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(19, 51)
+                        .addBox(-5.0F, 14.5F, -7.0F, 10.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(41, 16)
+                        .addBox(-6.0F, 14.5F, 5.0F, 12.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(33, 13)
+                        .addBox(-6.0F, 14.5F, -6.0F, 12.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(41, 53)
+                        .addBox(-4.0F, 11.5F, -8.0F, 8.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 39)
+                        .addBox(7.0F, 11.5F, -4.0F, 1.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(52, 34)
+                        .addBox(-4.0F, 11.5F, 7.0F, 8.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(19, 35)
+                        .addBox(-8.0F, 11.5F, -4.0F, 1.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(52, 29)
+                        .addBox(-4.0F, 2.5F, -8.0F, 8.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 35)
+                        .addBox(7.0F, 2.5F, -4.0F, 1.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(52, 3)
+                        .addBox(-4.0F, 2.5F, 7.0F, 8.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 27)
+                        .addBox(-8.0F, 2.5F, -4.0F, 1.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(41, 31)
+                        .addBox(6.0F, 0.5F, -4.0F, 1.0F, 2.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(0, 57)
+                        .addBox(-4.0F, 0.5F, -7.0F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(41, 2)
+                        .addBox(-7.0F, 0.5F, -4.0F, 1.0F, 2.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(18, 54)
+                        .addBox(-4.0F, 0.5F, 6.0F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(60, 61)
+                        .addBox(-6.0F, 2.5F, -6.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(55, 61)
+                        .addBox(-5.0F, 2.5F, -7.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(50, 61)
+                        .addBox(-7.0F, 2.5F, -5.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(45, 61)
+                        .addBox(5.0F, 2.5F, -6.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(40, 61)
+                        .addBox(4.0F, 2.5F, -7.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(35, 61)
+                        .addBox(6.0F, 2.5F, -5.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(30, 61)
+                        .addBox(-7.0F, 2.5F, 4.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(25, 61)
+                        .addBox(-6.0F, 2.5F, 5.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(20, 61)
+                        .addBox(-5.0F, 2.5F, 6.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(15, 61)
+                        .addBox(4.0F, 2.5F, 6.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(10, 61)
+                        .addBox(5.0F, 2.5F, 5.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(5, 61)
+                        .addBox(6.0F, 2.5F, 4.0F, 1.0F, 12.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 24)
+                        .addBox(5.0F, 0.5F, 4.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(22, 23)
+                        .addBox(-5.0F, 0.5F, 5.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(18, 20)
+                        .addBox(-5.0F, 0.5F, -6.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 20)
+                        .addBox(5.0F, 0.5F, -5.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 14)
+                        .addBox(4.0F, 0.5F, 5.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 10)
+                        .addBox(-6.0F, 0.5F, 4.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 4)
+                        .addBox(-6.0F, 0.5F, -5.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+                        .addBox(4.0F, 0.5F, -6.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 10)
+                        .addBox(-6.0F, -0.5F, -4.0F, 12.0F, 1.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(48, 50)
+                        .addBox(-5.0F, -0.5F, -5.0F, 10.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(41, 26)
+                        .addBox(-5.0F, -0.5F, 4.0F, 10.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(19, 58)
+                        .addBox(-4.0F, -0.5F, 5.0F, 8.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(19, 20)
+                        .addBox(-8.0F, 5.5F, -4.0F, 1.0F, 6.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(0, 20)
+                        .addBox(7.0F, 5.5F, -4.0F, 1.0F, 6.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(49, 42)
+                        .addBox(-4.0F, 5.5F, -8.0F, 8.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(0, 47)
+                        .addBox(-4.0F, 5.5F, 7.0F, 8.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(0.0F, -7.5F, 0.0F));
 
         return LayerDefinition.create(meshDefinition, 128, 128);
     }
@@ -113,10 +110,9 @@ public class AgitatorModel extends Model {
         return this.parts;
     }
 
-    public record AgitatorParts(ModelPart structure, ModelPart glass, ModelPart rotor) {
+    public record AgitatorParts(ModelPart structure, ModelPart rotor) {
         public static AgitatorParts create(ModelPart root) {
-            return new AgitatorParts(root.getChild("structure"), root.getChild("structure").getChild("glass"),
-                    root.getChild("rotor"));
+            return new AgitatorParts(root.getChild("structure"), root.getChild("rotor"));
         }
     }
 }
