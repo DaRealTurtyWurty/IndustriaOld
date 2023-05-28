c2811dea97d2b5810e510d86faea4984f2063b5a
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2023-05-28 17:50:07 +0100

    Update Multiblock to reflect TurtyLib changes

--- src/main/java/dev/turtywurty/industria/init/MultiblockInit.java
+++ src/main/java/dev/turtywurty/industria/init/MultiblockInit.java
@@ -4,8 +4,8 @@ import dev.turtywurty.industria.Industria;
 import dev.turtywurty.industria.block.DistillationTowerBlock;
 import dev.turtywurty.turtylib.TurtyLib;
 import dev.turtywurty.turtylib.core.multiblock.Multiblock;
+import dev.turtywurty.turtylib.core.multiblock.modes.PlayerBuiltMultiblock;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.predicate.BlockStatePredicate;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -15,17 +15,15 @@ public class MultiblockInit {
             TurtyLib.MULTIBLOCK_REGISTRY_KEY, Industria.MODID);
 
     public static final RegistryObject<Multiblock> DISTILLATION_TOWER = MULTIBLOCKS.register("distillation_tower",
-            () -> new Multiblock(
-                    Multiblock.Builder.start()
-                            .aisle("IIII", "IIII", "IIII", "IIII")
-                            .aisle("AAAA", "AIIA", "AIIA", "AAAA")
-                            .aisle("AAAA", "AIIA", "AIIA", "AAAA")
-                            .aisle("AAAA", "AIIA", "AIIA", "AAAA")
-                            .where('I', BlockStatePredicate.forBlock(Blocks.IRON_BLOCK))
-                            .where('A', BlockBehaviour.BlockStateBase::isAir)
-                            .finish()
-                            .controller(0, 0, 0, BlockInit.DISTILLATION_TOWER.get().defaultBlockState())
-                            .useFunction((($0, level, blockPos, player, $1, $2, $3) -> DistillationTowerBlock.use(level, blockPos, player)))
-            ));
-
+            () -> PlayerBuiltMultiblock.Builder.start()
+                    .aisle("IIII", "IIII", "IIII", "IIII")
+                    .aisle("....", ".II.", ".II.", "....")
+                    .aisle("....", ".II.", ".II.", "....")
+                    .aisle("....", ".II.", ".II.", "....")
+                    .ignore('.')
+                    .where('I', BlockStatePredicate.forBlock(Blocks.IRON_BLOCK))
+                    .finish()
+                    .controller(0, 0, 0, BlockInit.DISTILLATION_TOWER.get().defaultBlockState())
+                    .useFunction((($0, level, blockPos, player, $1, $2, $3) -> DistillationTowerBlock.use(level, blockPos, player))).build()
+    );
 }
